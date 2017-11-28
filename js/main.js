93f83a9bff0c96278f39de15f09ae1fe18690d29
Aline <alinerambier@gmail.com>
2017-11-28 14:30:43 +0100

    Mise en forme

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+   /* Gestion burger menu */
+   /* Ouveture / Fermeture nav */
+    $('.burger').click(function(evt) {
+       evt.preventDefault();
+        $('header div').slideToggle('slow',function(){
+        });
+    });
+
+    /* Gestion retour haut de page */
+    var offset = 150;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.top').fadeIn(duration);
+        } else {
+            $('.top').fadeOut(duration);
+        }
+    });
+        
+    $('.top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    });
+});// Fin chargement DOM 
